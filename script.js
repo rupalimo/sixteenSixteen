582342e90298593cc994302adc7bef32262a311f
rupali <36826017+rupalimo@users.noreply.github.com>
2025-02-27 18:19:40 -0500

    add in tab system for archive and printed section

--- script.js
+++ script.js
@@ -13,3 +13,44 @@ $(document).ready(function () {
 		$(selectedClass).show();
 	});
 });
+
+// Archive Tabs
+function archiveToggle(evt, tabName) {
+	var archiveSection = document.getElementsByClassName('archive__container');
+	for (var i = 0; i < archiveSection.length; i++) {
+		archiveSection[i].style.display = 'none';
+	}
+
+	// Get all elements with class="archiveTab" and remove the class "active"
+	archiveTab = document.getElementsByClassName('archiveTab');
+	for (i = 0; i < archiveTab.length; i++) {
+		archiveTab[i].className = archiveTab[i].className.replace(' active', '');
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = 'block';
+	evt.currentTarget.className += ' active';
+}
+
+document.getElementById('defaultOpen').click();
+
+
+// Printed Tabs
+function printedToggle(evt, tabName) {
+	var printedSection = document.getElementsByClassName('printed__container');
+	for (var i = 0; i < printedSection.length; i++) {
+		printedSection[i].style.display = 'none';
+	}
+
+	// Get all elements with class="printedTab" and remove the class "active"
+	printedTab = document.getElementsByClassName('printedTab');
+	for (i = 0; i < printedTab.length; i++) {
+		printedTab[i].className = printedTab[i].className.replace(' active', '');
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = 'block';
+	evt.currentTarget.className += ' active';
+}
+
+document.getElementById('defaultOpen').click();

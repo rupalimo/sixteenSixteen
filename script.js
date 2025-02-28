da5a2da9f9d4ce71ff7b8c883125faf46e468292
rupali <36826017+rupalimo@users.noreply.github.com>
2025-02-27 19:04:12 -0500

    update style, tab headers

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function archiveToggle(evt, tabName) {
 	evt.currentTarget.className += ' active';
 }
 
-document.getElementById('defaultOpen').click();
+document.getElementById('defaultArchive').click();
 
 
 // Printed Tabs
@@ -53,4 +53,25 @@ function printedToggle(evt, tabName) {
 	evt.currentTarget.className += ' active';
 }
 
-document.getElementById('defaultOpen').click();
+document.getElementById('defaultPrinted').click();
+
+
+// Artists Tabs
+function artistsToggle(evt, tabName) {
+	var artistsSection = document.getElementsByClassName('artists__container');
+	for (var i = 0; i < artistsSection.length; i++) {
+		artistsSection[i].style.display = 'none';
+	}
+
+	// Get all elements with class="printedTab" and remove the class "active"
+	artistsTab = document.getElementsByClassName('artistsTab');
+	for (i = 0; i < artistsTab.length; i++) {
+		artistsTab[i].className = printedTab[i].className.replace(' active', '');
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = 'block';
+	evt.currentTarget.className += ' active';
+}
+
+document.getElementById('defaultArtists').click();
